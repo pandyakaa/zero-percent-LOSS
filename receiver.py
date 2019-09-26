986c76865b198948f52734dcc741dcb798a8b7d8
pandyakaa <pandyaka.aptanagi@gmail.com>
2019-09-26 19:59:26 +0700

    restructured program(again)'

--- receiver.py
+++ receiver.py
@@ -22,109 +22,52 @@ def bindParam():
 
     return port
 
-# Fungsi untuk parse ulang diagram
-def parseDiagram(list):
+# Fungsi untuk checkSum
+def checkSum(list):
 
-    # Start parsing
-    p_id = list[0] >> 4  # Parsing for Packet ID
+   checkOdd = 0
+   checkEven = 0
+   for i in range(8,max_packet_size) :
+      if (i%2 == 0) :
+         checkEven = checkEven ^ list[i]
+      else :
+         checkOdd = checkOdd ^ list[i]
+   
+   checkOdd = checkOdd ^ list[1] + list[3] + list[5]
+   checkEven = checkEven ^ list[0] + list[2] + list[4]
 
-    p_type = list[1] >> 4  # Parsing for Packet Type
-
-    p_seq = (list[2] << 8) + list[3]  # Parsing for Packet Sequence
-    p_length = (list[4] << 8) + list[5]  # Parsing for Packet Length
-    p_data = list[8:max_packet_size]  # Parsing for Packet Data
-    p_data = p_data.decode().rstrip('\x00')
-    p_data = p_data.encode()
-    p_cheksum = (list[6] << 8) + list[7]
-
-    return p_id, p_type, p_seq, p_length, p_data, p_cheksum
-
-# Fungsi untuk mencari checksum
-def getChecksum(p_id, p_type, p_seq, p_length, p_data):
-    diagram = bytearray(max_packet_size)
-
-    diagram[0] = ((p_id << 4) & 255)
-    diagram[1] = ((p_type << 4) & 255)
-    diagram[2] = (p_seq >> 8)  # SEQUENCE NUMBER
-    diagram[3] = p_seq & 255  # SEQUENCE NUMBER
-    diagram[4] = (p_length >> 8)  # LENGTH
-    diagram[5] = p_length & 255  # LENGTH
-
-    # Assign data to diagram from index 8
-    i = 8
-    for b in p_data:
-        diagram[i] = b
-        i += 1
-
-    # Assign checksum to diagram with index 7 and 8
-    sumEven = 0
-    sumOdd = 0
-    for i in range(8, max_packet_size):
-        if (i % 2 == 0):
-            sumEven = sumEven ^ diagram[i]
-        else:
-            sumOdd = sumOdd ^ diagram[i]
-
-    diagram[6] = diagram[0] ^ diagram[2] ^ diagram[4] ^ sumEven
-    diagram[7] = diagram[1] ^ diagram[3] ^ diagram[5] ^ sumOdd
-
-    checksum = (diagram[6] << 8) + diagram[7]
-    return checksum
+   return (checkEven, checkOdd == list[6], list[7])
 
 # Fungsi receiveData()
 def receiveData():
+   
+   # Create UDP socket
+   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+   port = bindParam()
 
-    res = [[] for i in range(5)]
-    filename = []
+   sock.bind((localIP,port))
 
-    # Create UDP Socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+   while True :
+      data, addr = sock.recvfrom(max_packet_size)
+      seqVal = (data[2] << 8) + data[3]
 
-    # Call bindParam() function
-    port = bindParam()
+      if (seqVal == 1) :
+         filename = bytes(data[8:max_packet_size]).decode().rstrip('\00') + '-result.txt'
+         print('nama file ' + filename)
+         f = open(filename,'w')
+      else :
+         f.write(bytes(data[8:max_packet_size]).decode().rstrip('\x00'))
+         print('Paket ke ' + str(seqVal-1))
+         print('File ke ' + str(data[0]))
 
-    # Binding sock with localIP and port
-    sock.bind((localIP, port))
+      if checkSum(data) :
+         ack = Packet(data[0], 'ACK', seqVal, ''.encode())
+         sock.sendto(ack.getDiagram(),addr)
 
-    # Receive data
-
-    # Receive filename first
-    while True:
-        data, addr = sock.recvfrom(max_packet_size)
-        if data:
-            print('File name : ' + data.decode())
-            file_name = data.decode().strip()
-            filename.append(file_name)
-
-        # Receive the data
-        while True:
-            ready = select.select([sock], [], [], timeout)
-            if ready[0]:
-                data, addr = sock.recvfrom(max_packet_size)
-                if data:
-                    p_id, p_type, p_seq, p_length, p_data, p_checksum = parseDiagram(data)
-                    if (p_checksum == getChecksum(p_id, p_type, p_seq, p_length, p_data)):
-                        p = Packet(p_id, 'ACK', p_seq, ''.encode())
-                        sock.sendto(p.getDiagram(), addr)
-                        print('ACK for packet ' + str(p_id) + ' sent')
-                        res[p_id].append(p_data)
-                    else:
-                        print('checksum not valid')
-            else:
-                #print(file_name + ' finished')
-                break
-        break
-
-    return res, filename
-
+   f.close()
 
 def main():
-    res, filename = receiveData()
-    with open('result.txt', 'wb') as f:
-        for i in res:
-            for j in i:
-               f.write(j)
-
+    res = receiveData()
 
 if __name__ == "__main__":
     main()
