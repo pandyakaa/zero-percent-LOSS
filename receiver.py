a7991fd75eaf26f5ed8b7713d75a2b095258bd8b
afv <13517057@std.stei.itb.ac.id>
2019-09-26 18:04:25 +0700

    ignore

--- receiver.py
+++ receiver.py
@@ -13,76 +13,117 @@ localIP = 'localhost'
 timeout = 3
 max_packet_size = 33000
 
-# Fungsi bindParam() 
-def bindParam() :
-   port = input('Masukkan port yang akan di-bind : ')
+# Fungsi bindParam()
+def bindParam():
+    port = input('Masukkan port yang akan di-bind : ')
 
-   #Cast port to integer
-   port = int(port)
+    # Cast port to integer
+    port = int(port)
 
-   return port
+    return port
 
 # Fungsi untuk parse ulang diagram
-def parseDiagram(list) :
-   max_size_packet = 33000
+def parseDiagram(list):
 
-   # Start parsing
-   p_id = list[0] >> 4 # Parsing for Packet ID
+    # Start parsing
+    p_id = list[0] >> 4  # Parsing for Packet ID
 
-   p_type = list[1] >> 4 # Parsing for Packet Type
-   if p_type == 0 :
-      p_type = 'DATA'
-   elif p_type == 1 :
-      p_type = 'ACK'
-   elif p_type == 2 :
-      p_type = 'FIN'
-   elif p_type == 3 :
-      p_type = 'FIN-ACK'
+    p_type = list[1] >> 4  # Parsing for Packet Type
 
-   p_seq = (list[2] << 8) + list[3] # Parsing for Packet Sequence
-   p_length = (list[4] << 8) + list[5] # Parsing for Packet Length
-   p_data = bytes(list[8:max_size_packet+1]) # Parsing for Packet Data
-   p_data = p_data.decode().rstrip('\x00') # Parsing for Packet Data
+    p_seq = (list[2] << 8) + list[3]  # Parsing for Packet Sequence
+    p_length = (list[4] << 8) + list[5]  # Parsing for Packet Length
+    p_data = bytes(list[8:max_packet_size])  # Parsing for Packet Data
+    p_cheksum = (list[6] << 8) + list[7]
 
-   return p_id,p_type,p_seq,p_length,p_data
+    return p_id, p_type, p_seq, p_length, p_data, p_cheksum
+
+# Fungsi untuk mencari checksum
+def getChecksum(p_id, p_type, p_seq, p_length, p_data):
+    diagram = bytearray(max_packet_size)
+
+    diagram[0] = ((p_id << 4) & 255)
+    diagram[1] = ((p_type << 4) & 255)
+    diagram[2] = (p_seq >> 8)  # SEQUENCE NUMBER
+    diagram[3] = p_seq & 255  # SEQUENCE NUMBER
+    diagram[4] = (p_length >> 8)  # LENGTH
+    diagram[5] = p_length & 255  # LENGTH
+
+    # Assign data to diagram from index 8
+    i = 8
+    for b in p_data:
+        diagram[i] = b
+        i += 1
+
+    # Assign checksum to diagram with index 7 and 8
+    sumEven = 0
+    sumOdd = 0
+    for i in range(8, max_packet_size):
+        if (i % 2 == 0):
+            sumEven = sumEven ^ diagram[i]
+        else:
+            sumOdd = sumOdd ^ diagram[i]
+
+    diagram[6] = diagram[0] ^ diagram[2] ^ diagram[4] ^ sumEven
+    diagram[7] = diagram[1] ^ diagram[3] ^ diagram[5] ^ sumOdd
+
+    checksum = (diagram[6] << 8) + diagram[7]
+    return checksum
 
 # Fungsi receiveData()
-def receiveData() :
+def receiveData():
 
-   # Create UDP Socket
-   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    res = [[] for i in range(5)]
+    filename = []
 
-   # Call bindParam() function
-   port = bindParam()
+    # Create UDP Socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-   # Binding sock with localIP and port
-   sock.bind((localIP,port))
+    # Call bindParam() function
+    port = bindParam()
 
-   # Receive data and write file
-   with open('result.out','wb') :
+    # Binding sock with localIP and port
+    sock.bind((localIP, port))
 
-      # Receive filename first
-      while True :
-         data, addr = sock.recvfrom(max_packet_size)
-         #if data : 
-            #print('File name : ' + data.decode())
-            #file_name = data.decode().strip()
-         
-         # Receive the data
-         while True :
-            ready = select.select([sock],[],[],timeout)
-            if ready[0] :
-               data, addr = sock.recvfrom(max_packet_size)
-               p_id, p_type, p_seq, p_length, p_data = parseDiagram(data)
-               p = Packet(p_id,'ACK',p_seq,'asd'.encode())
-               sock.sendto(p.getDiagram(),addr)
-               print('ACK for packet ' + str(p_id) + ' sent')
-            else :
-               #print(file_name + ' finished')
-               break
+    # Receive data
 
-def main() :
-   receiveData()
+    # Receive filename first
+    while True:
+        data, addr = sock.recvfrom(max_packet_size)
+        if data:
+            print('File name : ' + data.decode())
+            file_name = data.decode().strip()
+            filename.append(file_name)
+
+        # Receive the data
+        while True:
+            ready = select.select([sock], [], [], timeout)
+            if ready[0]:
+                data, addr = sock.recvfrom(max_packet_size)
+                if data:
+                    p_id, p_type, p_seq, p_length, p_data, p_checksum = parseDiagram(data)
+                    if (p_checksum == getChecksum(p_id, p_type, p_seq, p_length, p_data)):
+                        p = Packet(p_id, 'ACK', p_seq, ''.encode())
+                        sock.sendto(p.getDiagram(), addr)
+                        print('ACK for packet ' + str(p_id) + ' sent')
+                        print(p_seq)
+                        res[p_id].append(p_data)
+                    else:
+                        print('checksum not valid')
+            else:
+                #print(file_name + ' finished')
+                break
+        break
+
+    return res, filename
+
+
+def main():
+    res, filename = receiveData()
+    with open(filename[0], 'wb') as f:
+        for i in res:
+            for j in i:
+               f.write(j)
+
 
 if __name__ == "__main__":
-   main()
+    main()
--- sender.py
+++ sender.py
@@ -10,10 +10,12 @@ from packet import Packet
 
 # Constant variable
 max_data_size = 32768
-max_packet_size = 33000 # max_data_size + 2 checksum + 2 length + 2 sequence number + 0.5 type + 0.5 ID + x error
+# max_data_size + 2 checksum + 2 length + 2 sequence number + 0.5 type + 0.5 ID + x error
+max_packet_size = 33000
 
 # Fungsi input IP, port dan filename
-def bindParam() :
+
+def bindParam():
 
     recv_ip = input("Masukkan IP dari address yang dituju : ")
     recv_port = input("Masukkan port dari address yang dituju : ")
@@ -29,66 +31,69 @@ def bindParam() :
     return recv_ip, recv_port, file_name
 
 # Fungsi untuk parse ulang diagram
-def parseDiagram(list) :
-   max_size_packet = 33000
+def parseDiagram(list):
 
-   # Start parsing
-   p_id = list[0] >> 4 # Parsing for Packet ID
+    # Start parsing
+    p_id = list[0] >> 4  # Parsing for Packet ID
 
-   p_type = list[1] >> 4 # Parsing for Packet Type
-   if p_type == 0 :
-      p_type = 'DATA'
-   elif p_type == 1 :
-      p_type = 'ACK'
-   elif p_type == 2 :
-      p_type = 'FIN'
-   elif p_type == 3 :
-      p_type = 'FIN-ACK'
+    p_type = list[1] >> 4  # Parsing for Packet Type
+    if p_type == 0:
+        p_type = 'DATA'
+    elif p_type == 1:
+        p_type = 'ACK'
+    elif p_type == 2:
+        p_type = 'FIN'
+    elif p_type == 3:
+        p_type = 'FIN-ACK'
 
-   p_seq = (list[2] << 8) + list[3] # Parsing for Packet Sequence
-   p_length = (list[4] << 8) + list[5] # Parsing for Packet Length
-   p_data = bytes(list[8:max_size_packet+1]) # Parsing for Packet Data
-   p_data = p_data.decode().rstrip('\x00') # Parsing for Packet Data
+    p_seq = (list[2] << 8) + list[3]  # Parsing for Packet Sequence
+    p_length = (list[4] << 8) + list[5]  # Parsing for Packet Length
+    p_data = bytes(list[8:max_packet_size+1])  # Parsing for Packet Data
+    p_data = p_data.decode().rstrip('\x00')  # Parsing for Packet Data
+    p_checksum = (list[6] << 8) + list[7]
 
-   return p_id,p_type,p_seq,p_length,p_data
+    return p_id, p_type, p_seq, p_length, p_data, p_checksum
 
 # Fungsi sendData
-def sendData() :
+def sendData():
 
     # Create UDP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    
+
     # Call bindParam() function
     recv_ip, recv_port, file_name = bindParam()
 
     # Send file_name to receiver
     # TODO : create progress bar
-    sock.sendto(file_name,(recv_ip,recv_port))
+    sock.sendto(file_name, (recv_ip, recv_port))
     file_name = file_name.decode()
     print('Sending ' + file_name + ' ...')
-    
+
     # Read file and send the content
-    with open(file_name,'rb') as f :
+    with open(file_name, 'rb') as f:
         data = f.read(max_data_size)
         idx = 1
         seq = 1
 
-        while(data) :
-            p = Packet(idx,'DATA', seq, data)
-            if (sock.sendto(p.getDiagram(),(recv_ip,recv_port))) :
+        while(data):
+            p = Packet(idx, 'DATA', seq, data)
+            if (sock.sendto(p.getDiagram(), (recv_ip, recv_port))):
                 data = f.read(max_data_size)
                 time.sleep(0.02)
             datarcv, addr = sock.recvfrom(max_data_size+1000)
-            if datarcv :
-                p_id, p_type, p_seq, p_length, p_data = parseDiagram(datarcv)
+            if datarcv:
+                p_id, p_type, p_seq, p_length, p_data, p_checksum = parseDiagram(
+                    datarcv)
                 print('ACK from packet ' + str(p_id) + ' Received')
-            seq+=1
-        
+            seq += 1
+
     sock.close()
     f.close()
 
-def main() :
+
+def main():
     sendData()
+
 
 if __name__ == "__main__":
     main()
